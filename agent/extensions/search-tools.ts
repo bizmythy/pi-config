31fe44cae844c46bccb4099122e5ab0daf25ed23
bizmythy <andrew.p.council@gmail.com>
2026-05-08 15:03:42 -0400

    Revert "enable built-in find and grep tools"
    This reverts commit 7edb3e576e9ef4e9b1f8f51e6c807a6d8654ffd2.

--- agent/extensions/search-tools.ts
+++
@@ -1,9 +0,0 @@
-import type { ExtensionAPI } from "@earendil-works/pi-coding-agent";
-import { createFindTool, createGrepTool } from "@earendil-works/pi-coding-agent";
-
-export default function (pi: ExtensionAPI) {
-  const cwd = process.cwd();
-
-  pi.registerTool(createGrepTool(cwd));
-  pi.registerTool(createFindTool(cwd));
-}
